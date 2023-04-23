c66b286e65588776b607f0c462ee338221dde120
Arjunraj Kokkadan <arjunrajkokkadan@gmail.com>
2023-04-23 13:39:45 +0530

    Complaint

--- lib/ui/screens/home_screen_sections/complaints_screen.dart
+++ lib/ui/screens/home_screen_sections/complaints_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:petsmart_admin/blocs/complaint/complaint_bloc.dart';
 import 'package:petsmart_admin/ui/widgets/complaints/complaint_card.dart';
+import 'package:petsmart_admin/ui/widgets/custom_progress_indicator.dart';
+
+import '../../widgets/custom_alert_dialog.dart';
 
 class ComplaintsScreen extends StatefulWidget {
   const ComplaintsScreen({super.key});
@@ -9,32 +14,64 @@ class ComplaintsScreen extends StatefulWidget {
 }
 
 class _ComplaintsScreenState extends State<ComplaintsScreen> {
+  final ComplaintBloc complaintBloc = ComplaintBloc();
+  @override
+  void initState() {
+    super.initState();
+    complaintBloc.add(GetAllComplaintEvent());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: SizedBox(
-        width: 1000,
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            Expanded(
-              child: SingleChildScrollView(
-                child: Wrap(
-                  spacing: 20,
-                  runSpacing: 20,
-                  children: List<Widget>.generate(
-                    10,
-                    (index) => const ComplaintCard(),
-                  ),
+      child: BlocProvider<ComplaintBloc>.value(
+        value: complaintBloc,
+        child: BlocConsumer<ComplaintBloc, ComplaintState>(
+          listener: (context, state) {
+            if (state is ComplaintFailureState) {
+              showDialog(
+                context: context,
+                builder: (context) => CustomAlertDialog(
+                  title: 'Failure',
+                  message: state.message,
+                  primaryButtonLabel: 'Ok',
                 ),
+              );
+            }
+          },
+          builder: (context, state) {
+            return SizedBox(
+              width: 1000,
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Expanded(
+                    child: state is ComplaintSuccessState
+                        ? state.complaints.isNotEmpty
+                            ? ListView.separated(
+                                itemBuilder: (context, index) => ComplaintCard(
+                                  complaintDetails: state.complaints[index],
+                                ),
+                                separatorBuilder: (context, index) =>
+                                    const SizedBox(height: 10),
+                                itemCount: state.complaints.length,
+                              )
+                            : const Center(
+                                child: Text('No complaints found'),
+                              )
+                        : const Center(
+                            child: CustomProgressIndicator(),
+                          ),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                ],
               ),
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-          ],
+            );
+          },
         ),
       ),
     );
--- lib/ui/screens/home_screen_sections/suggestions_screen.dart
+++ lib/ui/screens/home_screen_sections/suggestions_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:petsmart_admin/ui/widgets/suggestions/suggestion_card.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:petsmart_admin/ui/widgets/custom_progress_indicator.dart';
+
+import '../../../blocs/suggestion/suggestion_bloc.dart';
+import '../../widgets/custom_alert_dialog.dart';
+import '../../widgets/suggestions/suggestion_card.dart';
 
 class SuggestionsScreen extends StatefulWidget {
   const SuggestionsScreen({super.key});
@@ -9,32 +14,64 @@ class SuggestionsScreen extends StatefulWidget {
 }
 
 class _SuggestionsScreenState extends State<SuggestionsScreen> {
+  final SuggestionBloc complaintBloc = SuggestionBloc();
+  @override
+  void initState() {
+    super.initState();
+    complaintBloc.add(GetAllSuggestionEvent());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: SizedBox(
-        width: 1000,
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            Expanded(
-              child: SingleChildScrollView(
-                child: Wrap(
-                  spacing: 20,
-                  runSpacing: 20,
-                  children: List<Widget>.generate(
-                    10,
-                    (index) => const SuggestionCard(),
-                  ),
+      child: BlocProvider<SuggestionBloc>.value(
+        value: complaintBloc,
+        child: BlocConsumer<SuggestionBloc, SuggestionState>(
+          listener: (context, state) {
+            if (state is SuggestionFailureState) {
+              showDialog(
+                context: context,
+                builder: (context) => CustomAlertDialog(
+                  title: 'Failure',
+                  message: state.message,
+                  primaryButtonLabel: 'Ok',
                 ),
+              );
+            }
+          },
+          builder: (context, state) {
+            return SizedBox(
+              width: 1000,
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Expanded(
+                    child: state is SuggestionSuccessState
+                        ? state.suggestions.isNotEmpty
+                            ? ListView.separated(
+                                itemBuilder: (context, index) => SuggestionCard(
+                                  suggestionDetails: state.suggestions[index],
+                                ),
+                                separatorBuilder: (context, index) =>
+                                    const SizedBox(height: 10),
+                                itemCount: state.suggestions.length,
+                              )
+                            : const Center(
+                                child: Text('No suggestions found'),
+                              )
+                        : const Center(
+                            child: CustomProgressIndicator(),
+                          ),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                ],
               ),
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-          ],
+            );
+          },
         ),
       ),
     );
